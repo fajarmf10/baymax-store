6210b71e2163f143843677b833dba693a7ece2ed
Fajar Maulana Firdaus <Fajar.Firdaus@btpn.com>
2020-01-16 14:01:34 +0700

    Initial Commit

---
+++ src/test/java/com/fajar/learnspring/LearnspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fajar.learnspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
